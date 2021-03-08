0b14540b30a96e009e79723c4cf0d721fe8b8001
jano <77622056+Devjano@users.noreply.github.com>
2021-03-08 08:24:35 -0800

    Add files via upload

---
+++ plus.js
@@ -0,0 +1,133 @@
+const discord = require('discord.js');
+const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const ayarlar = require('./ayarlar.json');
+const app = express();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', async () => {
+   client.appInfo = await client.fetchApplication();
+  setInterval( async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+  
+ client.user.setActivity(`Darknes Code V12 Gif Bot`, { type:"WATHING" })
+  console.log("[BOT] Kullanıma Hazır!")
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require('./util/eventLoader.js')(client);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
+
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+           reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+client.login(ayarlar.token)
+
+
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+
+client.on("userUpdate", async(Revenge, NYKS) => {
+  
+if(Revenge.avatarURL === NYKS.avatarURL) return;
+let RevengePP = NYKS.avatarURL.split("?")[0]
+
+const Kanal = client.channels.filter(Revenge => Revenge.name === 'random-gif' && Revenge.type === 'text').array().forEach(Nucceteere => {
+if (RevengePP.endsWith('.gif')) {
+Nucceteere.send(new Discord.Attachment(RevengePP))
+}
+const Kanal2 = client.channels.filter(Revenge => Revenge.name === 'random-pp' && Revenge.type === 'text').array().forEach(Revenge => {
+if (!RevengePP.endsWith('.gif')) {
+Revenge.send(new Discord.Attachment(RevengePP))
+}
+})
+})
+})
+
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
+//------------------------ Halka Açık Random Gif Pp -----------------------//
